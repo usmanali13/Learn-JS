659f4b479432fa41ed2f78e3e6172b1ea39e8a04
Usman Ali <157132466+usmanali13@users.noreply.github.com>
2024-06-24 06:51:41 +0000

    while and do while loop

---
+++ 05_iterations/02_while.js
@@ -0,0 +1,42 @@
+//video 28 
+
+//while and do-while loop
+
+let i = 0
+
+while (i <= 10) {
+    console.log(`value of index is ${index}`);
+    i = i+2
+}
+
+//while loop with array
+
+let myArray = ["usman" , "ali", "rehan"]
+
+let arr = 0
+
+while (arr < myArray.length) {
+    console.log(   `Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+//do-while loop
+
+let score = 1
+
+do {
+    console.log(`score is ${score}`);
+    score++
+}
+while (score <= 10);
+
+//if score value is 11
+
+let scores = 11  //only one time loop run
+
+do {
+    console.log(`score is ${scores}`);
+    scores++
+}
+while (scores <= 10);
+
